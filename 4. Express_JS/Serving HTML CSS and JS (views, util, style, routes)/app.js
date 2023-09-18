31997156ae99e15775afdeb4901e2cc713d8ad5c
Ashish-17CSE <68491332+Ashish-17CSE@users.noreply.github.com>
2023-09-19 03:29:42 +0530

    Project files.

---
+++ 4. Express_JS/Serving HTML CSS and JS (views, util, style, routes)/app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const app = express();
+
+const adminRoutes = require('./Routes/admins.js');
+const shopRoutes = require('./Routes/shop.js')
+const helpRoutes = require('./Routes/help.js')
+
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use(express.static(path.join(__dirname, 'style')))
+
+app.use('/admins', adminRoutes);
+app.use(shopRoutes);
+app.use('/contactust', helpRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+
+
+app.listen(3000);
