9815d98e3ab1db543f2b4338a72b0133f28ba991
Ashish-17CSE <ashish.as28121998@gmail.com>
2023-09-21 00:35:03 +0530

    workig on Add Cart

--- 4. Express_JS/Dynamic Routes/controllers/shop.js
+++ 4. Express_JS/Dynamic Routes/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -37,6 +38,14 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/card');
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
