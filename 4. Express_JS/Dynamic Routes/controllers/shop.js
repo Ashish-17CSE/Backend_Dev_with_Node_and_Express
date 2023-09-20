c3294c07337e0c03da87b2fc1dfaa3dc57ee60e3
Ashish-17CSE <ashish.as28121998@gmail.com>
2023-09-21 04:15:41 +0530

    workig on AddCart and Delete

--- 4. Express_JS/Dynamic Routes/controllers/shop.js
+++ 4. Express_JS/Dynamic Routes/controllers/shop.js
@@ -33,10 +33,22 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
-  });
+  Cart.getCart(cart => {
+    Product.fetchAll(product => {
+      const cartProducts = [];
+      for(product of product) {
+        const cardProductData = cart.products.find(prod => prod.id === product.id);
+        if(cardProductData){
+          cartProducts.push({productData: product, qty: cardProductData})
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      });
+    })
+  })
 };
 
 exports.postCart = (req, res, next) => {
@@ -45,6 +57,14 @@ exports.postCart = (req, res, next) => {
     Cart.addProduct(prodId, product.price);
   });
   res.redirect('/card');
+}
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price)
+    res.redirect('/cart')
+  })
 }
 
 exports.getOrders = (req, res, next) => {
--- 4. Express_JS/Dynamic Routes/models/cart.js
+++ 4. Express_JS/Dynamic Routes/models/cart.js
@@ -8,7 +8,7 @@ const p = path.join(
 )
 
 
-module.exports = class Cart { 
+module.exports = class Cart {
 
     static addProduct(id, productPrice) {
         // Fetch the previous cart
@@ -38,6 +38,39 @@ module.exports = class Cart {
                 console.log(err);
             });
         });
+    }
 
+    static deleteProduct(id, productPrice) {
+        fs.readFile(p, (err, fileContent) => {
+            if(err){
+                return;
+            }
+            const  updatedCart = {...JSON.parse(fileContent)};
+            const product = updatedCart.products.find(prod => prod.id  === id);
+            if(!product){
+                return;
+            }
+            
+            const productQty = product.qty;
+            updatedCart.products= updatedCart.products.filter(
+                prod => prod.id != id
+            );
+            updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+
+            fs.writeFile(p, JSON.stringify(updatedCart), err => {
+                console.log(err);
+            })
+        })
+    }
+
+    static getCart(cb) {
+        fs.readFile(p, (err, fileContent) => {
+            const cart = JSON.parse(fileContent);
+            if(err) {
+                cb(null);
+            }else{
+                cb(cart);
+            }
+        })
     }
 }
