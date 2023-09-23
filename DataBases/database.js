81c0e9c4a1e56a2adce341e4c2f83d8de519bd95
Ashish-17CSE <68491332+Ashish-17CSE@users.noreply.github.com>
2023-09-23 07:13:40 +0530

    database.js

--- DataBases/database.js
+++ DataBases/database.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user : 'root',
+    user: 'root',
     database: 'node-complete',
-    password: 'Ashish'
+    password: '12345'
 
 })
 
