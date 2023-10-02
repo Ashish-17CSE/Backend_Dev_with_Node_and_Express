d20eab0fb2ea9b4b1184e667ec4a328b1c1579ba
Ashish-17CSE <ashish.as28121998@gmail.com>
2023-10-03 03:36:46 +0530

    One to many relationship between User and Product

--- 4. Express_JS/Dynamic Routes/app.js
+++ 4. Express_JS/Dynamic Routes/app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User= require('./models/user');
 
 const app = express();
 
@@ -19,16 +21,35 @@ const { log } = require('console');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+    .then(user => {
+        req.user = user;
+        next();
+    }).catch(err => console.log(err));
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, {constraints: true, onDelete: "CASCADE"});
+User.hasMany(Product);
+
 sequelize
+    // .sync({force: true})
     .sync()
     .then(result => {
-        // console.log(result);
-        app.listen(4000);
+        return User.findByPk(1);
+    }).then( user => {
+        if(!user){
+            return User.create({name:"Max", email:"maxtest@test.com"});
+        }
+        return user;
+    }).then(user => {
+        // console.log(user);
+        app.listen(3000);
     })
     .catch(err => console.log(err))
 
--- 4. Express_JS/Dynamic Routes/controllers/admin.js
+++ 4. Express_JS/Dynamic Routes/controllers/admin.js
@@ -13,14 +13,12 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title: title,
     price: price,
     imageUrl : imageUrl,
     description:description
-  })
-  .then( result =>  {
-    // console.log(result);
+  }).then( result =>  {
     console.log('Created Product');
     res.redirect('/admin/products');
   })
@@ -34,8 +32,11 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(product => {
+  req.user
+    .getProducts({where: {id: prodId}})
+  // Product.findById(prodId)
+    .then(products => {
+      const product = products[0];
       if (!product) {
         return res.redirect('/');
       }
@@ -71,7 +72,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+ req.user.getProducts()
     .then(products => {
       res.render('admin/products', {
         prods: products,
