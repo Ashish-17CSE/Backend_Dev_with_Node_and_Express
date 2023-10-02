2b4c37f3deff6ca40ff2cda0c3faba2521821ae1
Ashish-17CSE <ashish.as28121998@gmail.com>
2023-10-02 15:59:31 +0530

    Added bodyParser.JSON()

--- Booking App/app.js
+++ Booking App/app.js
@@ -10,10 +10,12 @@ const app = express();
 
 app.use(cors());
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-// app.use(bodyParser.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+app.use(bodyParser.json({ extended: false }));
+
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, 'public')));
 
