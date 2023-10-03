bc281d8a69440c7e475e48748d6513319c181326
Ashish-17CSE <ashish.as28121998@gmail.com>
2023-10-03 05:30:15 +0530

    Cart with many to many relationship

--- 4. Express_JS/Dynamic Routes/app.js
+++ 4. Express_JS/Dynamic Routes/app.js
@@ -8,6 +8,9 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User= require('./models/user');
+const Cart= require('./models/cart');
+const CartItem= require('./models/cart-item');
+
 
 const app = express();
 
@@ -36,6 +39,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, {constraints: true, onDelete: "CASCADE"});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
 
 sequelize
     // .sync({force: true})
@@ -49,6 +56,8 @@ sequelize
         return user;
     }).then(user => {
         // console.log(user);
+        return user.createCart();
+    }).then(cart => {
         app.listen(3000);
     })
     .catch(err => console.log(err))
--- 4. Express_JS/Dynamic Routes/controllers/shop.js
+++ 4. Express_JS/Dynamic Routes/controllers/shop.js
@@ -43,40 +43,69 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  Cart.getCart(cart => {
-    Product.findAll(products => {
-      const cartProducts = [];
-      for (product of products) {
-        const cartProductData = cart.products.find(
-          prod => prod.id === product.id
-        );
-        if (cartProductData) {
-          cartProducts.push({ productData: product, qty: cartProductData.qty });
-        }
-      }
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: cartProducts
-      });
-    });
-  });
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts()
+        .then(products => {
+          res.render('shop/cart', {
+            path: '/cart',
+            pageTitle: 'Your Cart',
+            products: products
+          });
+        }).catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+  let fatchedCart;
+  let newQuantuty = 1;
+  req.user
+    .getCart()
+    .then(cart => {
+      fatchedCart = cart;
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then(products => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantuty = oldQuantity + 1;
+        return product;
+      }
+      return Product.findByPk(prodId)
+    })
+    .then(product => {
+      return fatchedCart.addProduct(product, {
+        through: { quantity: newQuantuty }
+      });
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.deleteProduct(prodId, product.price);
-    res.redirect('/cart');
-  });
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts({where: {id: prodId}});
+    })
+    .then(products => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
