974e464fcc82a14d652d48743a5e4d5b67ef4cd5
Ashish-17CSE <ashish.as28121998@gmail.com>
2023-10-09 18:56:13 +0530

    remove unused line

--- Booking App/routes/users.js
+++ Booking App/routes/users.js
@@ -10,21 +10,10 @@ router.get('/users', userController.getAllUsers);
 // Route for creating a new user
 router.post('/users', userController.createUser);
 
-// Route for editing a user
-// router.get('/users/:id', userController.getEditUser);
-// router.post('/users/:id', userController.postEditUser);
-
-// Route for deleting a user
-// router.delete('/users/:id', userController.deleteUser);
-
 // Route for editing a user by ID
 router.put('/users/:id', userController.editUser);
 
 // Route for deleting a user by ID
 router.post('/users/:id', userController.deleteUser);
-
-
-
-
 
 module.exports = router;
