bc0031b41890caf69b3619a551842049bfb78852
Ashish-17CSE <ashish.as28121998@gmail.com>
2023-10-16 03:32:10 +0530

    EJS templating engine

--- udemy_Node_Expresss_MongoDB/MyBlog/index.js
+++ udemy_Node_Expresss_MongoDB/MyBlog/index.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const path = require('path');
-
 const app = new express();
+
+const ejs = require('ejs');
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'))
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    // res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    res.render('index')
 });
 
 app.get("/about", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    // res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    res.render('about')
 });
 
 app.get("/post", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    // res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+     res.render('post')
 });
 
 app.get("/contact", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    // res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+     res.render('contact')
 });
 
 
